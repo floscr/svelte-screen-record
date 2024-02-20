cdb35224621584606d71aea89a3a92697c8c2d5a
Florian Schroedl <flo.schroedl@gmail.com>
2024-02-20 21:27:47 +0100

    Add xstate inspector

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,15 @@
 <script lang="ts">
     import { createBrowserInspector } from "@statelyai/inspect";
     import { useMachine } from "@xstate/svelte";
-    import { Button, Select } from "flowbite-svelte";
-    import { onMount } from "svelte";
-    import { match } from "ts-pattern";
 
     import { stateMachine, StateNames } from "./state";
 
-    const { snapshot, state, send } = useMachine(stateMachine);
-    console.log(state);
-    console.log(snapshot);
-
     const { inspect } = createBrowserInspector({
         // Comment out the line below to start the inspector
-        autoStart: false,
+        // autoStart: false,
     });
 
-    // const view = match(state.context)
-    //     .with({ name: StateNames.Initial }, () => <p>Loading</p>)
-    //     .otherwise(() => null);
+    const { snapshot } = useMachine(stateMachine, { inspect });
 </script>
 
 <main>
