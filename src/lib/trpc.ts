12949b20b941a594cbb7f2f1eeee8d8dc7b6033a
Florian Schroedl <flo.schroedl@gmail.com>
2024-02-20 13:53:43 +0100

    Remove boilerplate

--- src/lib/trpc.ts
+++
@@ -1,9 +0,0 @@
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
-import superjson from "superjson";
-
-import type { AppRouter } from "../server/trpc";
-
-export const client = createTRPCProxyClient<AppRouter>({
-  links: [httpBatchLink({ url: "/trpc" })],
-  transformer: superjson,
-});
