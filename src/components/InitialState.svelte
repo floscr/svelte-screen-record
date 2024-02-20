de101176be3e0bdc9c70034340672d7b1c1747f1
Florian Schroedl <flo.schroedl@gmail.com>
2024-02-21 00:15:47 +0100

    Remove unneeded check

--- src/components/InitialState.svelte
+++ src/components/InitialState.svelte
@@ -6,21 +6,15 @@
     export let context: InitialState;
 </script>
 
-{#if context.devices}
-    <main>
-        <Select id="microphones" class="mt-2" placeholder="Select a Microphone">
-            {#each context.devices.audioDevices as { label, deviceId }}
-                <option value={deviceId}
-                    >{label || deviceId || "Default"}</option
-                >
-            {/each}
-        </Select>
-        <Select id="cameras" class="mt-2" placeholder="Select a Camera">
-            {#each context.devices.videoDevices as { label, deviceId }}
-                <option value={deviceId}
-                    >{label || deviceId || "Default"}</option
-                >
-            {/each}
-        </Select>
-    </main>
-{/if}
+<main>
+    <Select id="microphones" class="mt-2" placeholder="Select a Microphone">
+        {#each context.devices.audioDevices as { label, deviceId }}
+            <option value={deviceId}>{label || deviceId || "Default"}</option>
+        {/each}
+    </Select>
+    <Select id="cameras" class="mt-2" placeholder="Select a Camera">
+        {#each context.devices.videoDevices as { label, deviceId }}
+            <option value={deviceId}>{label || deviceId || "Default"}</option>
+        {/each}
+    </Select>
+</main>
