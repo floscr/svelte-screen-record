27b7e570390bfc98c9a33ce422fb76e771529b96
Florian Schroedl <flo.schroedl@gmail.com>
2024-02-20 16:08:01 +0100

    Indent

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,23 +1,23 @@
-import { WebSocketServer } from "ws";
 import fs from "fs";
 import { randomUUID } from "node:crypto";
+import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", (ws) => {
-  const id = randomUUID();
-  const fileName = `${id}.mp4`;
-  const writeStream = fs.createWriteStream(fileName);
+    const id = randomUUID();
+    const fileName = `${id}.mp4`;
+    const writeStream = fs.createWriteStream(fileName);
 
-  console.log(`Client connected, creating stream file`, fileName);
+    console.log(`Client connected, creating stream file`, fileName);
 
-  ws.on("message", (message) => {
-    console.log("Received chunk");
-    writeStream.write(message);
-  });
+    ws.on("message", (message) => {
+        console.log("Received chunk");
+        writeStream.write(message);
+    });
 
-  ws.on("close", () => {
-    console.log("Client disconnected");
-    writeStream.end();
-  });
+    ws.on("close", () => {
+        console.log("Client disconnected");
+        writeStream.end();
+    });
 });
