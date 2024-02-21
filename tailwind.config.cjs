6c17e7d951cba5f6312cec7ff14c587f95e0c2fe
Florian Schroedl <flo.schroedl@gmail.com>
2024-02-21 12:40:52 +0100

    Expose tailwind colors as variables

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,32 +1,37 @@
 const config = {
-  content: [
-    "./src/**/*.{html,js,svelte,ts}",
-    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
-  ],
+    content: [
+        "./src/**/*.{html,js,svelte,ts}",
+        "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}",
+    ],
 
-  plugins: [require("flowbite/plugin")],
+    plugins: [
+        require("flowbite/plugin"),
+        require("@tailwind-plugin/expose-colors")({
+            extract: ["primary"],
+        }),
+    ],
 
-  darkMode: "class",
+    darkMode: "class",
 
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          bg: "#202736",
-          "50": "#f8fafc",
-          "100": "#f1f5f9",
-          "200": "#e2e8f0",
-          "300": "#cbd5e1",
-          "400": "#94a3b8",
-          "500": "#64748b",
-          "600": "#475569",
-          "700": "#334155",
-          "800": "#1e293b",
-          "900": "#0f172a",
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    bg: "#202736",
+                    "50": "#f8fafc",
+                    "100": "#f1f5f9",
+                    "200": "#e2e8f0",
+                    "300": "#cbd5e1",
+                    "400": "#94a3b8",
+                    "500": "#64748b",
+                    "600": "#475569",
+                    "700": "#334155",
+                    "800": "#1e293b",
+                    "900": "#0f172a",
+                },
+            },
         },
-      },
     },
-  },
 };
 
 module.exports = config;
